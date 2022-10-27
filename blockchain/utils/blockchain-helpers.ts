d01e444bed83bd8900920e1093806b6f1a3ee040
KowalewskiPawel <pawelkowalewsk@gmail.com>
2022-10-27 22:47:47 +0200

    exports changed

--- blockchain/utils/blockchain-helpers.ts
+++ blockchain/utils/blockchain-helpers.ts
@@ -2,66 +2,49 @@ import { readFileSync, writeFileSync } from "fs";
 import { Blockchain, Transaction, Wallets } from "../types";
 
 /** blockchain helpers **/
-const getBlockchain = (): Blockchain => {
+export const getBlockchain = (): Blockchain => {
   const blockchainFile = readFileSync("./blockchain/blockchain.json");
   const blockchain = JSON.parse(String(blockchainFile));
   return blockchain;
 }
 
-const writeBlockchain = (blockchain: Blockchain): void => {
+export const writeBlockchain = (blockchain: Blockchain): void => {
   const blockchainString = JSON.stringify(blockchain, null, 2);
   writeFileSync("./blockchain/blockchain.json", blockchainString);
 }
 
-const _getBlockchain = getBlockchain;
-export { _getBlockchain as getBlockchain };
-const _writeBlockchain = writeBlockchain;
-export { _writeBlockchain as writeBlockchain };
-
 /** transaction helpers **/
-const getTransactions = (): Transaction[] => {
+export const getTransactions = (): Transaction[] => {
   const transactionsFile = readFileSync("./blockchain/transactions.json");
   const transactions = JSON.parse(String(transactionsFile));
   return transactions;
 }
 
-const writeTransactions = (transactions: Transaction[]): void => {
+export const writeTransactions = (transactions: Transaction[]): void => {
   const transactionsString = JSON.stringify(transactions, null, 2);
   writeFileSync("./blockchain/transactions.json", transactionsString);
 }
 
-const _getTransactions = getTransactions;
-export { _getTransactions as getTransactions };
-const _writeTransactions = writeTransactions;
-export { _writeTransactions as writeTransactions };
-
 /** wallet helpers **/
-const getWallets = (): Wallets => {
+export const getWallets = (): Wallets => {
   const walletsFile = readFileSync("./blockchain/wallets.json");
   const wallets = JSON.parse(String(walletsFile));
   return wallets;
 }
 
-const writeWallets = (wallets: Wallets): void => {
+export const writeWallets = (wallets: Wallets): void => {
   const walletsString = JSON.stringify(wallets, null, 2);
   writeFileSync("./blockchain/wallets.json", walletsString);
 }
 
-const getWalletAddressFromName = (name: string): string => {
+export const getWalletAddressFromName = (name: string): string => {
   const wallets = getWallets();
   if(!wallets[name]) throw Error("There is no such a name on the wallet list!");
   return wallets[name].publicKey;
 }
 
-const _getWallets = getWallets;
-export { _getWallets as getWallets };
-const _writeWallets = writeWallets;
-export { _writeWallets as writeWallets };
-const _getWalletAddressFromName = getWalletAddressFromName;
-export { _getWalletAddressFromName as getWalletAddressFromName };
-
 /** address helpers **/
-const getAddressBalance = (address: string): number => {
+export const getAddressBalance = (address: string): number => {
   const blockchain = getBlockchain();
   const transactions = getTransactions();
   let balance = 0;
@@ -100,6 +83,3 @@ const getAddressBalance = (address: string): number => {
 
   return balance;
 }
-
-const _getAddressBalance = getAddressBalance;
-export { _getAddressBalance as getAddressBalance };
