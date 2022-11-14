7a2c5c5f858f1dcdc4a4ab42d1cc75b69f3c61b3
lachDev <p.kowalewski@projectnexus.app>
2022-11-14 22:47:19 +0100

    transfer added

--- blockchain/transfer.ts
+++ blockchain/transfer.ts
@@ -1,36 +1,37 @@
-import { MINIMUM_GAS_FEE } from './consts';
+import { MINIMUM_GAS_FEE } from "./consts";
 import {
-    getAddressBalance,
-    getTransactions,
-    writeTransactions,
-    ec
-  } from './utils';
-  
-  const senderPrivateKey = process.argv[2];
-  const amount = Number(process.argv[3]);
-  const gasFee = Number(process.argv[4]);
+  getAddressBalance,
+  getTransactions,
+  writeTransactions,
+  ec,
+} from "./utils";
 
-  if(!gasFee || gasFee < MINIMUM_GAS_FEE) throw Error("Gas fee is too low");
+export const transfer = (
+  senderPrivateKey: string,
+  amount: number,
+  gasFee: number,
+  receiverPublicKey: string
+) => {
+  if (!gasFee || gasFee < MINIMUM_GAS_FEE) throw Error("Gas fee is too low");
 
-  const receiverPublicKey = process.argv[5];
   const currentTransactions = getTransactions();
-  
+
   const senderKeypair = ec.keyFromPrivate(senderPrivateKey);
-  const senderAddress = senderKeypair.getPublic('hex');
-  
+  const senderAddress = senderKeypair.getPublic("hex");
+
   const signature = senderKeypair.sign(senderAddress + amount).toDER("hex");
-  
+
   const transaction = {
     senderAddress,
     receiverAddress: receiverPublicKey,
     amount,
     gasFee,
-    signature
+    signature,
   };
-  
-  const isBalanceEnough = getAddressBalance(senderAddress) >= (amount + gasFee);
-  
-  if(!isBalanceEnough) throw Error("Not enough tokens!");
-  
+
+  const isBalanceEnough = getAddressBalance(senderAddress) >= amount + gasFee;
+
+  if (!isBalanceEnough) throw Error("Not enough tokens!");
+
   writeTransactions([...currentTransactions, transaction]);
-  
+};
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { generateWallet, initBlockchain, mineBlockchain } from "./blockchain";
-import { getBlockchain, getWallets } from "./blockchain/utils";
+import { generateWallet, initBlockchain, mineBlockchain, transfer } from "./blockchain";
+import { getBlockchain, getTransactions, getWallets } from "./blockchain/utils";
 import { initPubSub, broadcastChain } from "./pubsub";
 
 const app = express();
@@ -9,24 +9,44 @@ generateWallet("default");
 initBlockchain();
 
 initPubSub();
-setInterval(() => { 
-    const blockchain = getBlockchain();
-    broadcastChain(blockchain);
-}, 10000);
-
 
 const wallets = getWallets();
 const minerPrivateKey = wallets.default.privateKey;
 
+setInterval(() => { 
+    const transactions = getTransactions();
+    if(transactions.length) {
+        mineBlockchain(minerPrivateKey);
+    }
+    const blockchain = getBlockchain();
+    broadcastChain(blockchain);
+}, 10000);
+
 app.use(bodyParser.json());
 
 app.post("/api/mine", (req, res) => {
-  try {
-    mineBlockchain(minerPrivateKey);
-    const blockchainNew = getBlockchain();
-    broadcastChain(blockchainNew);
+    try {
+      mineBlockchain(minerPrivateKey);
+      const blockchainNew = getBlockchain();
+      broadcastChain(blockchainNew);
+  
+      return res.status(200).send({ success: true, message: "New Block mined" });
+    } catch (error) {
+      console.error("Block not mined", error);
+      return res.status(500).json({
+        error: true,
+        message: error
+      });
+    }
+  });
 
-    return res.status(200).send({ success: true, message: "New Block mined" });
+app.post("/api/transfer", (req, res) => {
+  try {
+    const { senderPrivateKey, amount, gasFee, receiverPublicKey } = req.body;
+
+    transfer(senderPrivateKey, amount, gasFee, receiverPublicKey);
+
+    return res.status(200).send({ success: true, message: "Transaction added to the mempool" });
   } catch (error) {
     console.error("Block not mined", error);
     return res.status(500).json({
